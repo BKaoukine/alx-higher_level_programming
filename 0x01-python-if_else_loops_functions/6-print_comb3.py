8c155f4ae5ed6fcc84fe0404f6d0ad2555d85312
BKaoukine <b.koukin@gmail.com>
2023-11-01 00:58:02 +0100

    Task 7

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+_1st_digite = 0
+
+while _1st_digite < 10:
+    _2nd_digite = _1st_digite + 1
+    while _2nd_digite < 10:
+        separator = '' if _1st_digite == 8 and _2nd_digite == 9 else ", "
+        print("{:d}{:d}".format(_1st_digite, _2nd_digite), end=separator)
+        _2nd_digite += 1
+    _1st_digite += 1
+print("\n")
