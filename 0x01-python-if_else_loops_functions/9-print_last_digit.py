35b1fa3cd6697edcfdf48e4518b5358f7f3e7131
BKaoukine <b.koukin@gmail.com>
2023-11-01 02:15:57 +0100

    FUN to find last digit

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    _last_digit = number - (10 * int(number / 10))
-    print("{:d}".format(abs(_last_digit)), end='')
+    _last_digit = abs(number - (10 * int(number / 10)))
+    print("{:d}".format(_last_digit), end='')
     return _last_digit
