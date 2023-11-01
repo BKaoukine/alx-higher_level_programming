39728e2331e29176036e9ac04baa5e1fe6406226
BKaoukine <b.koukin@gmail.com>
2023-11-01 01:38:59 +0100

    str to Uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,8 +3,8 @@ def uppercase(str):
     i = 0
     str_length = len(str)
     while i < str_length:
-        if 65 <= ord(str[i]) <= 90:
-            print("{:s}".format(chr((ord(str[i]) + 32))), end='')
+        if 97 <= ord(str[i]) <= 122:
+            print("{:s}".format(chr((ord(str[i]) - 32))), end='')
         else:
             print("{:s}".format(str[i]), end='')
         i += 1
