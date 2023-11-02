49873d99a552ad3325a2b0b982bb18f028b94a3a
BKaoukine <b.koukin@gmail.com>
2023-11-02 22:36:07 +0100

    adding the  Python idiom

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
-from add_0 import add
-a = 1
-b = 2
+if __name__ == "__main__":
+    """
+    This script prints the sum of 1 and 2
+    using the 'add' function from add_0.py.
+    """
 
-print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    from add_0 import add
+    a = 1
+    b = 2
+
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
