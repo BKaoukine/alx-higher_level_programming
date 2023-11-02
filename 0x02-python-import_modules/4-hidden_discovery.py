f07ec47cd5387c6d37fcbefe8f5fe668e6ae5fa6
BKaoukine <b.koukin@gmail.com>
2023-11-03 00:35:44 +0100

    Import a Compiled module

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-    for l in dir(hidden_4):
-        if l[0:2] != "__":
-            print(l)
+    for def_names in dir(hidden_4):
+        if def_names[0:2] != "__":
+            print(def_names)
