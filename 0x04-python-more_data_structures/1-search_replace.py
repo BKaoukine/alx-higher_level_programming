bcda9cb598f4ca166aafaf8bb7fe73303e89f30a
BKaoukine <b.koukin@gmail.com>
2023-11-09 00:30:44 +0100

    Search and replace

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,9 @@
+def search_replace(my_list, search, replace):
+    my_list_len = len(my_list)
+    index = 0
+    while index < my_list_len:
+        if my_list[index] == search:
+            my_list[index] = replace
+        index += 1
+        
+    return my_list
