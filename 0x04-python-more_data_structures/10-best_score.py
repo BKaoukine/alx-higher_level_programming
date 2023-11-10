40400638b946ec9bd09ae189db421ce2738c3f6c
BKaoukine <b.koukin@gmail.com>
2023-11-10 23:53:09 +0100

    Best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if a_dictionary:
-        best_score = next(iter(a_dictionary.values()), None)
-        for key in a_dictionary:
-            if a_dictionary[key] > best_score:
-                best_score = a_dictionary[key]
-        return best_score
+        best_score_value = next(iter(a_dictionary.values()), None)
+        best_score_key = None
+
+        for key, value in a_dictionary.items():
+            if value > best_score_value:
+                best_score_value = value
+                best_score_key = key
+
+        return best_score_key
     else:
         return None
