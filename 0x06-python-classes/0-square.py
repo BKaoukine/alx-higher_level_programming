24ecc58350fce21ec3351fb358833b8ac396d3d9
BKaoukine <b.koukin@gmail.com>
2023-11-21 18:46:42 +0100

    My first square

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,14 +1,8 @@
 #!/usr/bin/python3
+"""empty square class"""
+
+
 class Square:
-    """
-    The Square class represents a basic square in a two-dimensional space.
-
-    Attributes:
-    None
-
-    Methods:
-    None
-    """
 
     """pass"""
     pass
