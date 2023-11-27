2e075ce659ab7ac4ee959d966a3b82bd71490609
BKaoukine <b.koukin@gmail.com>
2023-11-27 22:49:37 +0100

    Change representation

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -5,6 +5,7 @@
 class Rectangle:
     """Rectangle Classe"""
     number_of_instances = 0
+    print_symbol = "#"
 
     def __init__(self, width=0, height=0):
         """initialization of instational
@@ -25,7 +26,7 @@ class Rectangle:
 
         for i in range(self.height):
             for j in range(self.width):
-                result += "#"
+                result += Rectangle.print_symbol
             if i < self.height - 1:
                 result += "\n"
         return result
