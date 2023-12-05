ae4a8d20f37891148fd565490183de8ff03b5053
BKaoukine <b.koukin@gmail.com>
2023-12-05 19:21:23 +0100

    Function that converts obj into JSON

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Function that converts a string into a JSON."""
+
+
+import json
+def to_json_string(my_obj):
+    """to_json_string taks one arg.
+
+    my_obj: the object to convert.
+    """
+    serialize_obj = json.dump(my_obj)
+    return serialize_obj
