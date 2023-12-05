e0e5372e9f527596eabaf8154af51c63d9d27f66
BKaoukine <b.koukin@gmail.com>
2023-12-06 00:24:39 +0100

    Load, add, save

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Script that adds all arguments to a Python list,
+ and then save them to a file.
+ """
+import json
+import sys
+module_name1 = '6_Load_from_json_file'
+load_from_json_file = getattr(__import__(module_name1), 'load_from_json_file')
+module_name2 = '5_save_to_json_file.py'
+save_to_json_file = getattr(__import__(module_name1), 'save_to_json_file')
+
+arg_list = []
+for arg in sys.argv:
+    arg_list.append(arg)
+
+save_to_json_file(arg_list, add_item.json)
+load_from_json_file(add_item.json)
