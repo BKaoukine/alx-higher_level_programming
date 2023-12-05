781b3494d3f266ed37f49aabf4b5e0123d59d39d
BKaoukine <b.koukin@gmail.com>
2023-12-05 18:44:03 +0100

    function that reads a text file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+"""Function that opens a file and read it's contecnt."""
+
+
+def read_file(filename=""):
+    """Open the filename as file."""
+    with open(filename) as file:
+        """Reade the file."""
+        file.read()
