c6bb50704e81f64cf771a848a3f9138167e3344b
BKaoukine <b.koukin@gmail.com>
2023-12-06 15:13:09 +0100

    Adding doctest file

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -10,14 +10,17 @@ class BaseGeometry:
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """Integer validator takes two arg.
-        
-        name : name of the variable.
-        value : value of the variable.
+        """Public instance method that validates an integer value.
+
+        Args:
+            name (str): The name of the variable.
+            value (int): The value of the variable.
+
+        Raises:
+            TypeError: If the value is not an integer.
+            ValueError: If the value is less than or equal to 0.
         """
-        self.name = name
-        self.value = value
-        if self.value != int:
-            raise TypeError("<name> must be an integer")
-        if self.value <= 0:
-            raise ValueError("<name> must be greater than 0")
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
