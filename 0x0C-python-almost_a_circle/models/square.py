b83a903537a3be52f5678e6700a5b017faec0652
BKaoukine <b.koukin@gmail.com>
2023-12-12 20:07:28 +0100

    __init__ modification.

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -8,9 +8,7 @@ class Square(Rectangle):
 
     def __init__(self, size, x=0, y=0, id=None):
         """Class Constructor."""
-        super().__init__(id, x, y)
-        super().__init__(width=size)
-        super().__init__(height=size)
+        super().__init__(size, size, x, y, id)
 
     @property
     def width(self):
