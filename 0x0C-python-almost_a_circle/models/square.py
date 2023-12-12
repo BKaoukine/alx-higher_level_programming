cb9cd84c586514c73c0659298306eefb20c9984b
BKaoukine <b.koukin@gmail.com>
2023-12-12 20:41:28 +0100

    Square size fix

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,65 +1,60 @@
 #!/usr/bin/python3
-"""Square Class."""
+"""square."""
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """Square Class that inherits from Rectangle."""
+    """Square."""
 
     def __init__(self, size, x=0, y=0, id=None):
-        """Class Constructor."""
+        """__init__.
+
+        :param size:
+        :param x:
+        :param y:
+        :param id:
+        """
         super().__init__(size, size, x, y, id)
 
     @property
-    def width(self):
-        """Get the width."""
-        return super().width
-
-    @width.setter
-    def width(self, size):
-        if type(size) is not int:
-            raise TypeError("width must be integer")
-        if size <= 0:
-            raise ValueError("width must be > 0")
-
-        self.width = size
-
-    @property
     def size(self):
-        """Get the size."""
+        """The size property."""
         return self.width
 
     @size.setter
     def size(self, value):
-        """Set the size, assigning the same value to width and height."""
-        if type(value) is not int:
-            raise TypeError("width must be integer")
-        if value <= 0:
-            raise ValueError("width must be > 0")
+        """size.
 
+        :param value:
+        """
         self.width = value
         self.height = value
 
     def update(self, *args, **kwargs):
-        """Update method to update the attributes."""
-        if args:
-            if len(args) >= 1:
-                self.id = args[0]
-            if len(args) >= 2:
-                self.size = args[1]
-            if len(args) >= 3:
-                self.x = args[2]
-            if len(args) >= 4:
-                self.y = args[3]
-        elif kwargs:
-            for key, value in kwargs.items():
+        """update.
+
+        :param args:
+        :param kwargs:
+        """
+        attributes = ['id', 'size', 'x', 'y']
+
+        for i, arg in enumerate(args):
+            if i < len(attributes):
+                setattr(self, attributes[i], arg)
+
+        for key, value in kwargs.items():
+            if key in attributes:
                 setattr(self, key, value)
 
     def to_dictionary(self):
-        """To_dictionary Creates a dic representation of the Class."""
-        return {
-            'id': self.id,
-            'size': self.size,
-            'x': self.x,
-            'y': self.y,
-        }
+        """to_dictionary."""
+        attr_dic = {}
+        attribute_names = ['id', 'size', 'x', 'y']
+        for attr in attribute_names:
+            attr_value = getattr(self, attr, None)
+            attr_dic[attr] = attr_value
+        return attr_dic
+
+    def __str__(self):
+        """__str__."""
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.height}"
