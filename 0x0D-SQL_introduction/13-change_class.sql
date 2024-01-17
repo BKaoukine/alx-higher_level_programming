19406b3260e5f44df7fa2b34de94e890fc934b9e
BKaoukine <b.koukin@gmail.com>
2024-01-17 18:54:11 +0100

    13. Score too low

---
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -0,0 +1,5 @@
+-- Script that updates the score of Bob to 10 in the table second_table.
+
+        -- You are not allowed to use Bob’s id value, only the name field
+
+DELETE FROM second_table WHERE score <= 5;
