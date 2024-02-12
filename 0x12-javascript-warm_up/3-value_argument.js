bfe40b573acaf28e8520d2512b03170967f6decd
BKaoukine <b.koukin@gmail.com>
2024-02-12 18:41:06 +0100

    4th taks

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
-const numArg = process.argv.length;
-if (numArg === 2) {
+if (process.argv[2] === undefined) {
   console.log('No argument');
 } else {
   console.log(process.argv[2]);
