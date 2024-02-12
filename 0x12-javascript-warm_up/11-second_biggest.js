77cabb6127e555d1988ea49b5f37f0c445715020
BKaoukine <b.koukin@gmail.com>
2024-02-12 23:13:13 +0100

    biggest number

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,17 +4,13 @@ const numarg = process.argv.length;
 if (numarg <= 3) {
   console.log(0);
 } else {
-  let maxnum;
-  let i = 0;
-  let j = 1;
-  while (i < numarg - 1) {
-    if (parseInt(process.argv[i]) > parseInt(process.argv[j])) {
+  let maxnum = parseInt(process.argv[2]);
+  let i = 2;
+  while (i < numarg) {
+    if (maxnum < parseInt(process.argv[i])) {
       maxnum = parseInt(process.argv[i]);
-    } else {
-      maxnum = parseInt(process.argv[j]);
     }
     i++;
-    j++;
   }
 
   console.log(maxnum);
