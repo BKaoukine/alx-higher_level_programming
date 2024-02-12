ad34a73ab01899bd20602ec9945f76c66580ae6e
BKaoukine <b.koukin@gmail.com>
2024-02-12 22:43:59 +0100

    factorial recursive

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const comput = parseInt(process.argv[2]);
+
+function factorial (i) {
+  if (i === 0 || i === 1) {
+    return 1;
+  } else {
+    return i * factorial(i - 1);
+  }
+}
+
+const Nfactorial = factorial(comput);
+console.log(Nfactorial);
