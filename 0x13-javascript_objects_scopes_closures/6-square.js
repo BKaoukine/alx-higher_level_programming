cef39ba1e13aabafc1ba1ff48a46f9962e73ed41
BKaoukine <b.koukin@gmail.com>
2024-02-13 21:23:54 +0100

    task 7

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -3,10 +3,6 @@
 const Square5 = require('./5-square.js');
 
 class Square extends Square5 {
-  constructor (size) {
-    super(size);
-  }
-
   charPrint (c) {
     if (c === undefined) {
       super.print();
@@ -14,7 +10,7 @@ class Square extends Square5 {
       for (let i = 0; i < this.height; i++) {
         let str = '';
         for (let j = 0; j < this.width; j++) {
-          str += 'C';
+          str += c;
         }
         console.log(str);
       }
