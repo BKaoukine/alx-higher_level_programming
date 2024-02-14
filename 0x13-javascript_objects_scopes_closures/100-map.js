18fffed8a70b56ea54e1a963dab27e4885fc9a28
BKaoukine <b.koukin@gmail.com>
2024-02-14 20:18:53 +0100

    map function

---
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+let importedlist = require('./100-data.js').list;
+
+let i = 0;
+
+const computedlist = importedlist.map((x) => x * i++);
+
+console.log(importedlist);
+console.log(computedlist);
