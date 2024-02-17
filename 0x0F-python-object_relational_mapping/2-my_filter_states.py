98f60ec799a354b333a75d220e3fee3d80d3aac2
bkaoukine <b.koukin@gmail.com>
2024-02-17 11:32:35 +0100

    Filter states by user input

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+A script to connect to a MySQL database and execute a query.
+
+Usage: python script.py <username> <password> <database> <query_column>
+
+Args:
+    username (str): MySQL username.
+    password (str): MySQL password.
+    database (str): MySQL database name.
+    query_column (str): Name of the column to query from the 'states' table.
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    """
+    Main function to connect to the MySQL database and execute a query.
+    """
+    # Establish a connection to the MySQL server
+    db = MySQLdb.connect(
+        host="localhost",
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        port=3306
+    )
+    cur = db.cursor()
+    
+    # Execute the SQL query to select specified column from the 'states' table
+    cur.execute(f"SELECT {sys.argv[4]} FROM states")
+    
+    # Fetch all the results
+    rows = cur.fetchall()
+    
+    # Print the results
+    for row in rows:
+        print(row)
+    
+    # Close the cursor and database connection
+    cur.close()
+    db.close()
