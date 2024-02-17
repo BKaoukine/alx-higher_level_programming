ef1ff37b66ff91347297ad898cba2e21215d981a
bkaoukine <b.koukin@gmail.com>
2024-02-17 11:40:30 +0100

    Filter states by user input

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -27,7 +27,7 @@ if __name__ == '__main__':
     )
     cur = db.cursor()
     
-    cur.execute(f"SELECT * FROM states WHERE name LIKE '{sys.argv[4]}' ORDER BY states.id ASC")
+    cur.execute("SELECT * FROM states WHERE name LIKE '{}' ORDER BY states.id".format(sys.argv[4]))
     
     rows = cur.fetchall()
     
