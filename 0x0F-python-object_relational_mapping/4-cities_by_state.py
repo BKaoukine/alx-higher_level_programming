1d44bbaa1fed9347ab6c8c1adb5aaca5e245ff6a
bkaoukine <b.koukin@gmail.com>
2024-02-17 19:14:56 +0100

    Cities by states

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
+"""
+A script to connect to a MySQL database and execute a query.
 
+Usage: python script.py <username> <password> <database>
+
+Args:
+    username (str): MySQL username.
+    password (str): MySQL password.
+    database (str): MySQL database name.
+"""
 
 import MySQLdb
 import sys
@@ -8,6 +17,12 @@ if __name__ == '__main__':
     """
     Main function to connect to the MySQL database and execute a query.
     """
+    # Check if correct number of arguments provided
+    if len(sys.argv) != 4:
+        print("Usage: python script.py <username> <password> <database>")
+        sys.exit(1)
+
+    # Connect to MySQL server
     db = MySQLdb.connect(
         host="localhost",
         user=sys.argv[1],
@@ -18,15 +33,15 @@ if __name__ == '__main__':
 
     cur = db.cursor()
 
-    # Execute SQL query with parameterized query
-    query = "SELECT * FROM cities WHERE name ORDER BY cities.id"
-
+    # Execute SQL query
+    query = "SELECT * FROM cities ORDER BY cities.id"
     cur.execute(query)
 
+    # Fetch and print results
     rows = cur.fetchall()
-
     for row in rows:
         print(row)
 
+    # Close cursor and database connection
     cur.close()
     db.close()
