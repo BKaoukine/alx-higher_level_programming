65f163c4f3b8c523a1e225ff3624a3e51ed4eb7f
bkaoukine <b.koukin@gmail.com>
2024-02-18 12:35:30 +0100

    filter by states

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""
+A script to connect to a MySQL database and execute a query.
+
+Usage: python script.py <username> <password> <database> <State>
+
+Args:
+    username (str): MySQL username.
+    password (str): MySQL password.
+    database (str): MySQL database name.
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    """
+    Main function to connect to the MySQL database and execute a query.
+    """
+    # Check if correct number of arguments provided
+    if len(sys.argv) != 5:
+        print("Usage: python script.py <username> <password> <database> <State>")
+        sys.exit(1)
+
+    # Connect to MySQL server
+    db = MySQLdb.connect(
+        host="localhost",
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        port=3306
+    )
+
+    cur = db.cursor()
+    stat_name = sys.argv[4]
+    # Execute SQL query
+    query = "SELECT DISTINCT c.name AS city_name\
+            FROM cities AS c\
+            JOIN states AS s ON c.state_id = s.id\
+            WHERE s.name = %s"
+
+    cur.execute(query, (stat_name,))
+
+    # Fetch and print results
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+    # Close cursor and database connection
+    cur.close()
+    db.close()
