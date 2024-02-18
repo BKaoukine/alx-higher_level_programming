d5dfdaeeea1102f28fbaa9ac3d2f0d868082266d
bkaoukine <b.koukin@gmail.com>
2024-02-18 19:00:52 +0100

    Get all states

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,47 +1,24 @@
 #!/usr/bin/python3
-
-
 """
-A module to define the State class and connect to.
-
-a MySQL database using SQLAlchemy.
-This module demonstrates how to use SQLAlchemy to define
-a class representing a state and connect to a MySQL database.
-
-Usage:
-    The module can be imported and used to interact with a MySQL database.
-
-Classes:
-    State: A class representing a state with attributes id and name.
+Contains State class and Base,
+an instance of declarative_base()
 """
 
-from sqlalchemy import Column, String, Integer
+from sqlalchemy import MetaData, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import create_engine
 
-# Create an instance of Base
-Base = declarative_base()
+mt_data = MetaData()
+Base = declarative_base(metadata=mt_data)
 
 
 class State(Base):
     """
-    A class representing a state.
-
-    Attributes:
-        id (int): The unique identifier for the state.
-        name (str): The name of the state.
+    Class that defines state
+    table: the name of the table.
+    id: the id
+    name: the name:with expression as target:
+        pass
     """
-
-    __tablename__ = "states"
-
-    id = Column(Integer, autoincrement=True, primary_key=True, nullable=False)
+    __tablename__ = 'states'
+    id = Column(Integer, unique=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
-
-
-# Create an engine to connect to the MySQL database
-engine = create_engine(
-    "mysql://username:password@localhost:3306/database_name"
-    )
-
-# Create the table in the database
-Base.metadata.create_all(engine)
