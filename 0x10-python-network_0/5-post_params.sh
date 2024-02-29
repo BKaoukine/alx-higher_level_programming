04c158ccb26ace0b67309864fd363352176008c0
bkaoukine <b.koukin@gmail.com>
2024-02-29 23:41:47 +0100

    5. cURL POST parameters

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Bash script that takes in a URL, sends a request to that URL.and displays the size of the body of the response
-curl -Ls -X POST -d '{"email": test@gmail.com, "subject": I will always be here for PLD}' "$1"
+curl -s -X POST -d '{"email": test@gmail.com, "subject": I will always be here for PLD}' "$1"
