c866c22192dbea584cba3706e1141954280b8691
bkaoukine <b.koukin@gmail.com>
2024-03-01 23:22:23 +0100

    1. Response header value #0

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -6,9 +6,9 @@ variable found in the header of the response.
 """
 import urllib.request
 import sys
+if __name__ == "__main__":
+    with urllib.request.urlopen(sys.argv[1]) as respons:
 
-with urllib.request.urlopen(sys.argv[1]) as respons:
+        header = respons.info().get('X-Request-Id')
 
-    header = respons.info().get('X-Request-Id')
-
-    print(header)
+        print(header)
