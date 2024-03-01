881b7895240b0923035fc232874a16d2b921d9ba
bkaoukine <b.koukin@gmail.com>
2024-03-01 22:55:28 +0100

    0. What's my status? #0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Python script that fetches https://alx-intranet.hbtn.io/status."""
+import urllib.request
+
+with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as respons:
+
+    body = respons.read()
+
+    print("Body response:")
+    print("   - type: " + str(type(body)))
+    print("   - content: " + str(body))
+    print("   - utf8 content: " + str(body.decode("utf-8")))
---
+++ 0x11-python-network_1/README.md
@@ -0,0 +1,1 @@
+This a Place Holder for Readme.
