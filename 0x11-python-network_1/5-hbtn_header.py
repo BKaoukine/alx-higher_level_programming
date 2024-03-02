1fa6f891f6560e8c8a251e7146268debbfde40ee
bkaoukine <b.koukin@gmail.com>
2024-03-02 14:11:03 +0100

    4. What's my status? #1

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -10,8 +10,7 @@ import sys
 
 url = sys.argv[1]
 
-
-req = requests.get(url)
-
-x_req_id = req.headers['X-Request-Id']
-print(x_req_id)
+if __name__ == "__main__":
+    req = requests.get(url)
+    x_req_id = req.headers['X-Request-Id']
+    print(x_req_id)
