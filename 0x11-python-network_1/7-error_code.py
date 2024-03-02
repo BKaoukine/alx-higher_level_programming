d132239d4d8c9a66e625a7b3dbecb1b1c842ac41
bkaoukine <b.koukin@gmail.com>
2024-03-02 18:59:28 +0100

    7. Error code #1

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
 
     resp = requests.get(url)
 
-    if resp.status_code > 400:
+    if resp.status_code >= 400:
         print("Error code: ", resp.status_code)
 
     else:
