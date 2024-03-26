eba217e727afe4e0bfd35fd459e5bc6ed97695a4
bkaoukine <b.koukin@gmail.com>
2024-03-26 18:38:47 +0000

    2. Status code

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -5,8 +5,8 @@ const url = process.argv[2];
 
 request.get(url, function (err, response) {
   if (err) {
-    console.error('error: ', response.message);
+    console.error(`Error: ${err.message}`);
   } else {
-    console.log('code: ', response.statusCode);
+    console.log(`code: ${response.statusCode}`);
   }
 });
