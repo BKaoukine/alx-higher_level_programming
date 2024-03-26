29b673d56505cbc33fd6a2733b181b90b21c520e
bkaoukine <b.koukin@gmail.com>
2024-03-26 17:49:27 +0000

    0. Readme

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Python script that takes in a letter, sends a POST to the URL.
+
+and displays the bodey or the error.
+"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+
+    
