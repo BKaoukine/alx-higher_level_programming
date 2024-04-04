f0e863573cc09d33e029a71d315c4f96ec3ce6c3
bkaoukine <b.koukin@gmail.com>
2024-04-04 01:20:17 +0000

    12. Say hello to everybody!

---
+++ 0x15-javascript-web_jquery/102-script.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const base_url = 'https://hellosalut.stefanbohacek.dev/';
+
+  $('#btn_translate').click(function () {
+    const user_language = $('input#language_code').val();
+    const url = `${base_url}?lang=${user_language}`;
+    $('#hello').empty();
+    $.get(url, function (response) {
+      $('#hello').append(response.hello);
+    });
+  });
+});
